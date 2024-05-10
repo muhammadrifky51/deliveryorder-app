f18ae83b2bd2ecddfb051276d8b37d9a4d63605e
Muhammad Rifky Y <53180898+muhammadrifky51@users.noreply.github.com>
2024-05-10 21:16:16 +0700

    Modify form

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -29,6 +29,18 @@
   ```
 -->
 <script src="https://cdn.tailwindcss.com"></script>
+<link rel="stylesheet" href="https://code.jquery.com/ui/1.13.3/themes/base/jquery-ui.css">
+<link rel="stylesheet" href="/resources/demos/style.css">
+<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+  <script src="https://code.jquery.com/ui/1.13.3/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#datepicker" ).datepicker({
+      changeMonth: true,
+      changeYear: true
+    });
+  } );
+  </script>
 <div class="min-h-full">
   <nav class="bg-slate-800">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -140,126 +152,70 @@ Jalan Ampera Raya &#13;&#10; Gedung B
             <p class="mt-1 text-sm leading-6 text-gray-600">Delivery order document information.</p>
 
             <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                <div class="sm:col-span-3">
-                <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">First name</label>
+                <div class="sm:col-span-2">
+                <x-input-label for="DONumber" class="block text-sm font-medium leading-6 text-gray-900">DO Number</x-input-label>
                 <div class="mt-2">
-                    <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    <x-text-input :disabled="true" type="text" name="DONumber" id="DONumber" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></x-text-input>
                 </div>
                 </div>
 
-                <div class="sm:col-span-3">
-                <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Last name</label>
+                <div class="sm:col-span-2">
+                <label for="PONumber" class="block text-sm font-medium leading-6 text-gray-900">PO Number</label>
                 <div class="mt-2">
-                    <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    <input type="text" name="PONumber" id="PONumber" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                 </div>
                 </div>
 
-                <div class="sm:col-span-4">
-                <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                <div class="sm:col-span-2">
+                <label for="datepicker" class="block text-sm font-medium leading-6 text-gray-900">Date</label>
                 <div class="mt-2">
-                    <input id="email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    <input type="text" name="datepicker" id="datepicker" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                 </div>
                 </div>
 
-                <div class="sm:col-span-3">
-                <label for="country" class="block text-sm font-medium leading-6 text-gray-900">Country</label>
+                <div class="sm:col-span-full">
+                <label for="notes" class="block text-sm font-medium leading-6 text-gray-900">Notes</label>
                 <div class="mt-2">
-                    <select id="country" name="country" autocomplete="country-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-                    <option>United States</option>
-                    <option>Canada</option>
-                    <option>Mexico</option>
+                    <textarea id="notes" name="notes" type="text" class="block row-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </textarea>
+                </div>
+                </div>
+
+                <div class="sm:col-span-2">
+                <label for="items" class="block text-sm font-medium leading-6 text-gray-900">Items</label>
+                <div class="mt-2">
+                    <select id="items" name="items" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
+                    <option>Item 1</option>
+                    <option>Item 2</option>
+                    <option>Item 3</option>
                     </select>
                 </div>
                 </div>
 
-                <div class="col-span-full">
-                <label for="street-address" class="block text-sm font-medium leading-6 text-gray-900">Street address</label>
+                <div class="col-span-2">
+                <label for="street-address" class="block text-sm font-medium leading-6 text-gray-900">Quantity</label>
                 <div class="mt-2">
-                    <input type="text" name="street-address" id="street-address" autocomplete="street-address" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    <input type="text" name="street-address" id="street-address"  class="block w-1/2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                 </div>
                 </div>
 
-                <div class="sm:col-span-2 sm:col-start-1">
-                <label for="city" class="block text-sm font-medium leading-6 text-gray-900">City</label>
-                <div class="mt-2">
-                    <input type="text" name="city" id="city" autocomplete="address-level2" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                <div class="col-span-1">
+                <div class="mt-2 pt-6">
+                    <button type="submit"  class="rounded-md bg-red-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    Remove
+                    </button>
                 </div>
                 </div>
 
-                <div class="sm:col-span-2">
-                <label for="region" class="block text-sm font-medium leading-6 text-gray-900">State / Province</label>
+                <div class="col-span-2">
                 <div class="mt-2">
-                    <input type="text" name="region" id="region" autocomplete="address-level1" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                </div>
-                </div>
-
-                <div class="sm:col-span-2">
-                <label for="postal-code" class="block text-sm font-medium leading-6 text-gray-900">ZIP / Postal code</label>
-                <div class="mt-2">
-                    <input type="text" name="postal-code" id="postal-code" autocomplete="postal-code" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    Add Item
+                    </button>
                 </div>
                 </div>
             </div>
             </div>
-
-            <div class="border-b border-gray-900/10 pb-12">
-            <h2 class="text-base font-semibold leading-7 text-gray-900">Notifications</h2>
-            <p class="mt-1 text-sm leading-6 text-gray-600">We'll always let you know about important changes, but you pick what else you want to hear about.</p>
-
-            <div class="mt-10 space-y-10">
-                <fieldset>
-                <legend class="text-sm font-semibold leading-6 text-gray-900">By Email</legend>
-                <div class="mt-6 space-y-6">
-                    <div class="relative flex gap-x-3">
-                    <div class="flex h-6 items-center">
-                        <input id="comments" name="comments" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
-                    </div>
-                    <div class="text-sm leading-6">
-                        <label for="comments" class="font-medium text-gray-900">Comments</label>
-                        <p class="text-gray-500">Get notified when someones posts a comment on a posting.</p>
-                    </div>
-                    </div>
-                    <div class="relative flex gap-x-3">
-                    <div class="flex h-6 items-center">
-                        <input id="candidates" name="candidates" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
-                    </div>
-                    <div class="text-sm leading-6">
-                        <label for="candidates" class="font-medium text-gray-900">Candidates</label>
-                        <p class="text-gray-500">Get notified when a candidate applies for a job.</p>
-                    </div>
-                    </div>
-                    <div class="relative flex gap-x-3">
-                    <div class="flex h-6 items-center">
-                        <input id="offers" name="offers" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
-                    </div>
-                    <div class="text-sm leading-6">
-                        <label for="offers" class="font-medium text-gray-900">Offers</label>
-                        <p class="text-gray-500">Get notified when a candidate accepts or rejects an offer.</p>
-                    </div>
-                    </div>
-                </div>
-                </fieldset>
-                <fieldset>
-                <legend class="text-sm font-semibold leading-6 text-gray-900">Push Notifications</legend>
-                <p class="mt-1 text-sm leading-6 text-gray-600">These are delivered via SMS to your mobile phone.</p>
-                <div class="mt-6 space-y-6">
-                    <div class="flex items-center gap-x-3">
-                    <input id="push-everything" name="push-notifications" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
-                    <label for="push-everything" class="block text-sm font-medium leading-6 text-gray-900">Everything</label>
-                    </div>
-                    <div class="flex items-center gap-x-3">
-                    <input id="push-email" name="push-notifications" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
-                    <label for="push-email" class="block text-sm font-medium leading-6 text-gray-900">Same as email</label>
-                    </div>
-                    <div class="flex items-center gap-x-3">
-                    <input id="push-nothing" name="push-notifications" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
-                    <label for="push-nothing" class="block text-sm font-medium leading-6 text-gray-900">No push notifications</label>
-                    </div>
-                </div>
-                </fieldset>
-            </div>
-            </div>
-
 
         <div class="mt-6 flex items-center justify-end gap-x-6">
             <a href="/deliveryorder-app/public" type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
